3d9a84f432f592be5fc2ae3c3195f653a55aa7fc
TheVigor <igor.proskuryakov14@gmail.com>
2018-09-12 22:31:56 +0300

    added bottom navigation view

--- app/src/main/java/com/noble/activity/instaclone/MainActivity.kt
+++ app/src/main/java/com/noble/activity/instaclone/MainActivity.kt
@@ -2,11 +2,21 @@ package com.noble.activity.instaclone
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottom_navigation_view.setIconSize(29f, 29f)
+        bottom_navigation_view.setTextVisibility(false)
+        bottom_navigation_view.enableItemShiftingMode(false)
+        bottom_navigation_view.enableShiftingMode(false)
+        bottom_navigation_view.enableAnimation(false)
+
+        for (i in 0 until bottom_navigation_view.menu.size())
+            bottom_navigation_view.setIconTintList(i, null)
     }
 }
