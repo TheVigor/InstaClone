888bb5cc48ddcb8a3356bea394fd113b75b8235c
TheVigor <igor.proskuryakov14@gmail.com>
2018-09-29 14:38:45 +0300

    added updating profile with firebase and password dialog

--- app/src/main/java/com/noble/activity/instaclone/activities/EditProfileActivity.kt
+++ app/src/main/java/com/noble/activity/instaclone/activities/EditProfileActivity.kt
@@ -3,16 +3,22 @@ package com.noble.activity.instaclone.activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 import com.noble.activity.instaclone.R
 import com.noble.activity.instaclone.models.User
+import com.noble.activity.instaclone.views.PasswordDialog
 import kotlinx.android.synthetic.main.activity_edit_profile.*
 
-class EditProfileActivity : AppCompatActivity() {
+class EditProfileActivity : AppCompatActivity(), PasswordDialog.Listener {
+
+    private lateinit var mUser: User
+    private lateinit var mPendingUser: User
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDatabase: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,22 +28,96 @@ class EditProfileActivity : AppCompatActivity() {
             finish()
         }
 
-        val auth = FirebaseAuth.getInstance()
-        val database = FirebaseDatabase.getInstance().reference
-        database.child("users").child(auth.currentUser!!.uid).addListenerForSingleValueEvent(
-                ValueEventListenerAdapter {
-                    val user = it.getValue(User::class.java)
+        save_image.setOnClickListener{
+            updateProfile()
+        }
 
-                    name_input.setText(user!!.name, TextView.BufferType.EDITABLE)
-                    username_input.setText(user.username, TextView.BufferType.EDITABLE)
-                    website_input.setText(user.website, TextView.BufferType.EDITABLE)
-                    bio_input.setText(user.bio, TextView.BufferType.EDITABLE)
-                    email_input.setText(user.email, TextView.BufferType.EDITABLE)
-                    phone_input.setText(user.phone.toString(), TextView.BufferType.EDITABLE)
+        mAuth = FirebaseAuth.getInstance()
+        mDatabase = FirebaseDatabase.getInstance().reference
+        mDatabase.child("users").child(mAuth.currentUser!!.uid).addListenerForSingleValueEvent(
+                ValueEventListenerAdapter {
+                    mUser = it.getValue(User::class.java)!!
+
+                    name_input.setText(mUser.name, TextView.BufferType.EDITABLE)
+                    username_input.setText(mUser.username, TextView.BufferType.EDITABLE)
+                    website_input.setText(mUser.website, TextView.BufferType.EDITABLE)
+                    bio_input.setText(mUser.bio, TextView.BufferType.EDITABLE)
+                    email_input.setText(mUser.email, TextView.BufferType.EDITABLE)
+                    phone_input.setText(mUser.phone.toString(), TextView.BufferType.EDITABLE)
 
                 })
 
+    }
 
+    private fun updateProfile() {
+
+        mPendingUser = User(
+                name = name_input.text.toString(),
+                username = username_input.text.toString(),
+                website = website_input.text.toString(),
+                bio = bio_input.text.toString(),
+                email = email_input.text.toString(),
+                phone = phone_input.text.toString().toLong()
+        )
+        val error = validate(mPendingUser)
+        if (error == null) {
+            if (mPendingUser.email == mUser.email) {
+                updateUser(mPendingUser)
+            } else {
+                PasswordDialog().show(supportFragmentManager, "password_dialog")
+
+            }
+        } else {
+            showToast(error)
+        }
+    }
+
+    private fun validate(user: User): String? =
+        when {
+            user.name.isEmpty() -> "Please enter name"
+            user.username.isEmpty() -> "Please enter username"
+            user.email.isEmpty() -> "Please enter email"
+            else -> null
+
+        }
+
+    private fun updateUser(user: User) {
+        val updatesMap = mutableMapOf<String, Any>()
+
+        if (user.name != mUser.name) updatesMap["name"] = user.name
+        if (user.username != mUser.username) updatesMap["username"] = user.username
+        if (user.website != mUser.website) updatesMap["website"] = user.website
+        if (user.bio != mUser.bio) updatesMap["bio"] = user.bio
+        if (user.email != mUser.email) updatesMap["email"] = user.email
+        if (user.phone != mUser.phone) updatesMap["phone"] = user.phone
+
+        mDatabase.child("users").child(mAuth.currentUser!!.uid).updateChildren(updatesMap)
+                .addOnCompleteListener{
+                    if (it.isSuccessful) {
+                        showToast("Profile saved")
+                        finish()
+                    } else {
+                        showToast(it.exception!!.message!!)
+                    }
+                }
+
+    }
+
+    override fun onPasswordConfirm(password: String) {
+        val credential = EmailAuthProvider.getCredential(mUser.email, password)
+        mAuth.currentUser!!.reauthenticate(credential).addOnCompleteListener{
+            if (it.isSuccessful) {
+                mAuth.currentUser!!.updateEmail(mPendingUser.email).addOnCompleteListener{
+                    if (it.isSuccessful) {
+                        updateUser(mPendingUser)
+                    } else {
+                        showToast(it.exception!!.message!!)
+                    }
+                }
+            } else {
+                showToast(it.exception!!.message!!)
+            }
+        }
     }
 }
 
---
+++ app/src/main/java/com/noble/activity/instaclone/views/PasswordDialog.kt
@@ -0,0 +1,37 @@
+package com.noble.activity.instaclone.views
+
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.support.v7.app.AlertDialog
+import com.noble.activity.instaclone.R
+import kotlinx.android.synthetic.main.dialog_password.view.*
+
+
+class PasswordDialog: DialogFragment() {
+    private lateinit var mListener: Listener
+    interface Listener {
+        fun onPasswordConfirm(password: String)
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        mListener = context as Listener
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val view = activity!!.layoutInflater.inflate(R.layout.dialog_password, null)
+        return AlertDialog.Builder(context!!)
+                .setView(view)
+                .setPositiveButton(android.R.string.ok, {_, _ ->
+                    mListener.onPasswordConfirm(view.password_input.text.toString())
+                })
+                .setNegativeButton(android.R.string.cancel, {_,_ ->
+
+                })
+                .setTitle(R.string.please_enter_password)
+                .create()
+    }
+}
