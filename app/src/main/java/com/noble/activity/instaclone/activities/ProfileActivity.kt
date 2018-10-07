4ff0ff8fdd75f37f6b2acf4fe89d52a0b4457563
TheVigor <igor.proskuryakov14@gmail.com>
2018-10-07 15:13:47 +0300

    started implementing share activity

--- app/src/main/java/com/noble/activity/instaclone/activities/ProfileActivity.kt
+++ app/src/main/java/com/noble/activity/instaclone/activities/ProfileActivity.kt
@@ -3,9 +3,16 @@ package com.noble.activity.instaclone.activities
 import android.content.Intent
 import android.os.Bundle
 import com.noble.activity.instaclone.R
+import com.noble.activity.instaclone.models.User
+import com.noble.activity.instaclone.utils.FirebaseHelper
+import com.noble.activity.instaclone.utils.ValueEventListenerAdapter
 import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : BaseActivity(4) {
+
+    private lateinit var mFirebaseHelper: FirebaseHelper
+
+    private lateinit var mUser: User
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +24,12 @@ class ProfileActivity : BaseActivity(4) {
             val intent = Intent(this, EditProfileActivity::class.java)
             startActivity(intent)
         }
+
+        mFirebaseHelper = FirebaseHelper(this)
+        mFirebaseHelper.currentUserReference().addValueEventListener(ValueEventListenerAdapter{
+            mUser = it.getValue(User::class.java)!!
+            profile_image.loadUserPhoto(mUser.photo)
+            username_text.text = mUser.username
+        })
     }
 }
--- app/src/main/java/com/noble/activity/instaclone/activities/ShareActivity.kt
+++ app/src/main/java/com/noble/activity/instaclone/activities/ShareActivity.kt
@@ -1,14 +1,34 @@
 package com.noble.activity.instaclone.activities
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import com.noble.activity.instaclone.R
+import com.noble.activity.instaclone.utils.CameraPictureTaker
+import com.noble.activity.instaclone.utils.GlideApp
+import kotlinx.android.synthetic.main.activity_share.*
 
 class ShareActivity : BaseActivity(2) {
 
+    private lateinit var mCameraPictureTaker: CameraPictureTaker
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_share)
 
-        setupBottomNavigation()
+        
+        mCameraPictureTaker = CameraPictureTaker(this)
+        mCameraPictureTaker.takeCameraPicture()
+
+        back_image.setOnClickListener{
+            finish()
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == mCameraPictureTaker.TAKE_PICTURE_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            GlideApp.with(this).load(mCameraPictureTaker.mImageUri).centerCrop().into(post_image)
+        }
     }
 }
