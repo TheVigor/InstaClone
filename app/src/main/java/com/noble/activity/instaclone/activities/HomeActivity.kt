a46ba3620a7f85bfa0cbafb44364b7a3fa504780
TheVigor <igor.proskuryakov14@gmail.com>
2018-10-07 23:42:08 +0300

    added creating posts and saving in firebase storage

--- app/src/main/java/com/noble/activity/instaclone/activities/HomeActivity.kt
+++ app/src/main/java/com/noble/activity/instaclone/activities/HomeActivity.kt
@@ -4,35 +4,43 @@ import android.content.Intent
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
 import com.noble.activity.instaclone.R
+import com.noble.activity.instaclone.utils.FirebaseHelper
+import com.noble.activity.instaclone.utils.ValueEventListenerAdapter
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : BaseActivity(0) {
     private val TAG = "HomeActivity"
 
-    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mFirebaseHelper: FirebaseHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         setupBottomNavigation()
 
-        mAuth = FirebaseAuth.getInstance()
+        mFirebaseHelper = FirebaseHelper(this)
 
         sign_out_text.setOnClickListener{
-            mAuth.signOut()
+            mFirebaseHelper.mAuth.signOut()
         }
-        mAuth.addAuthStateListener {
+        mFirebaseHelper.mAuth.addAuthStateListener {
             if (it.currentUser == null) {
                 startActivity(Intent(this, LoginActivity::class.java))
                 finish()
             }
         }
 
+        mFirebaseHelper.mDatabase.child("feed-posts").child(mFirebaseHelper.mAuth.currentUser!!.uid)
+                .addValueEventListener(ValueEventListenerAdapter{
+                    val posts = it.children.map{ it.getValue(FeedPost::class.java)!!}
+                    showToast(posts.size.toString())
+                })
+
     }
 
     override fun onStart() {
         super.onStart()
-        if (mAuth.currentUser == null) {
+        if (mFirebaseHelper.mAuth.currentUser == null) {
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }
--- app/src/main/java/com/noble/activity/instaclone/activities/ShareActivity.kt
+++ app/src/main/java/com/noble/activity/instaclone/activities/ShareActivity.kt
@@ -3,16 +3,22 @@ package com.noble.activity.instaclone.activities
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import com.google.firebase.database.ServerValue
 import com.noble.activity.instaclone.R
+import com.noble.activity.instaclone.models.User
 import com.noble.activity.instaclone.utils.CameraPictureTaker
 import com.noble.activity.instaclone.utils.FirebaseHelper
 import com.noble.activity.instaclone.utils.GlideApp
+import com.noble.activity.instaclone.utils.ValueEventListenerAdapter
 import kotlinx.android.synthetic.main.activity_share.*
+import java.sql.Timestamp
+import java.util.*
 
 class ShareActivity : BaseActivity(2) {
 
     private lateinit var mCameraPictureTaker: CameraPictureTaker
     private lateinit var mFirebaseHelper: FirebaseHelper
+    private lateinit var mUser: User
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +32,10 @@ class ShareActivity : BaseActivity(2) {
         back_image.setOnClickListener{ finish() }
         share_text.setOnClickListener{ share() }
 
+        mFirebaseHelper.currentUserReference().addValueEventListener(ValueEventListenerAdapter{
+           mUser = it.getValue(User::class.java)!!
+        })
+
     }
 
     private fun share() {
@@ -38,12 +48,21 @@ class ShareActivity : BaseActivity(2) {
             ref.putFile(imageUri).addOnCompleteListener{
                     if (it.isSuccessful) {
                         ref.downloadUrl.addOnCompleteListener {
+                            val imageDownloadUrl = it.result.toString()
                             mFirebaseHelper.mDatabase.child("images").child(uid).push()
-                                    .setValue(it.result.toString()).addOnCompleteListener{
+                                    .setValue(imageDownloadUrl).addOnCompleteListener{
                                         if (it.isSuccessful) {
-                                            startActivity(Intent(this,
-                                                    ProfileActivity::class.java))
-                                            finish()
+                                            mFirebaseHelper.mDatabase.child("feed-posts").child(uid)
+                                                    .push()
+                                                    .setValue(mkFeedPost(uid, imageDownloadUrl))
+                                                    .addOnCompleteListener{
+                                                        if (it.isSuccessful) {
+                                                            startActivity(Intent(this,
+                                                                    ProfileActivity::class.java))
+                                                            finish()
+                                                        }
+                                                    }
+
                                         } else {
                                             showToast(it.exception!!.message!!)
                                         }
@@ -57,6 +76,16 @@ class ShareActivity : BaseActivity(2) {
         }
     }
 
+    fun mkFeedPost(uid: String, imageDownloadUrl: String): FeedPost {
+        return FeedPost(
+                uid = uid,
+                username = mUser.username,
+                image = imageDownloadUrl,
+                caption = capture_input.text.toString(),
+                photo = mUser.photo
+        )
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == mCameraPictureTaker.TAKE_PICTURE_REQUEST_CODE) {
             if (resultCode == Activity.RESULT_OK) {
@@ -67,3 +96,13 @@ class ShareActivity : BaseActivity(2) {
         }
     }
 }
+
+data class FeedPost(val uid: String = "", val username: String = "", val photo: String? = null,
+                    val image: String = "",
+                    val likesCount: Int = 0, val commentsCount: Int = 0, val caption: String = "",
+                    val comments: List<Comment> = emptyList(),
+                    val timestamp: Any = ServerValue.TIMESTAMP) {
+    fun timestampDate(): Date = Date(timestamp as Long)
+}
+
+data class Comment(val uid: String, val username: String, val text: String)
